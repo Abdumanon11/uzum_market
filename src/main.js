1e05c19eb7dd943c27a5f916fd989fd40a459bf0
Abdumanon11 <abdumanonilhomov@gmail.com>
2025-08-09 14:44:23 +0500

    _

--- src/main.js
+++ src/main.js
@@ -1,26 +1,4 @@
 import axios from "axios";
-
-import jsonServer from "json-server";
-
-const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults();
-
-// Разрешаем CORS
-server.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(7777, () => {
-  console.log("JSON Server is running on port 7777");
-});
-
 const pages = [
 
   {
---
+++ src/server.js
@@ -0,0 +1,19 @@
+import jsonServer from "json-server";
+
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
+const middlewares = jsonServer.defaults();
+
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(7777, () => {
+  console.log("JSON Server is running on port 7777");
+});
